f3add1f6768741088c524dfe05aa2306804333c5
Aditya Narayan Dalapati <adityanarayandalapati@gmail.com>
2025-10-15 14:41:00 +0530

    Removed stutter in contact summary

--- src/sections/ContactSummary.jsx
+++ src/sections/ContactSummary.jsx
@@ -12,18 +12,18 @@ const ContactSummary = () => {
         "Excellence"
     ]
     const items2 = ["Contact me","Contact me","Contact me","Contact me","Contact me"]
-    useGSAP(()=>{
-        gsap.to(containerRef.current, {
-            scrollTrigger:{
-                trigger:containerRef.current,
-                start: "center center",
-                end: "+=800 center",
-                scrub: 0.5,
-                pin:true,
-                pinSpacing:true,
-            }
-        })
-    },[])
+    // useGSAP(()=>{
+    //     gsap.to(containerRef.current, {
+    //         scrollTrigger:{
+    //             trigger:containerRef.current,
+    //             start: "center center",
+    //             end: "+=800 center",
+    //             scrub: 0.5,
+    //             pin:true,
+    //             pinSpacing:true,
+    //         }
+    //     })
+    // },[])
   return (
     <section ref={containerRef} className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16">
         <Marquee items={items1} />
